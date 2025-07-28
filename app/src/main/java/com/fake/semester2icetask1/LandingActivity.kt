2139080508d76a3122b65dc543c80feff5745e0e
ST10375622 <128041756+ST10375622@users.noreply.github.com>
2025-07-28 14:59:35 +0200

    Initial commit

---
+++ app/src/main/java/com/fake/semester2icetask1/LandingActivity.kt
@@ -0,0 +1,36 @@
+package com.fake.semester2icetask1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LandingActivity : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_landing)
+
+
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
